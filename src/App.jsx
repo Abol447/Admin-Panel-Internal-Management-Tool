e0e5d5e4b8919a413f03589e04f82b055b941538
Abolfazl Bolouki 447 <160716723+Abol447@users.noreply.github.com>
2025-08-27 06:55:45 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center bg-red-900 min-h-screen font-bold text-white text-3xl">
+      sds
+    </div>
+  );
+}
+
+export default App;
