75bef7abd9ff22e2ebf1b1748dacdeba732de24e
Abolfazl Bolouki 447 <160716723+Abol447@users.noreply.github.com>
2025-09-01 12:08:23 +0330

    review

--- src/component/Layout.jsx
+++ src/component/Layout.jsx
@@ -5,13 +5,12 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div className="bg-gray-200" >
+    <div className="bg-[#F1F1F1]">
       <Navbar />
 
-      <div className="flex bg-[#F1F1F1] pt-4">
+      <div className="flex gap-4 px-4 pt-4">
         <Sidebar />
         <Outlet />
-
       </div>
     </div>
   );
