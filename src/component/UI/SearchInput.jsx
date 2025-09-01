044a9fe4b458fd6dc3fe81f13958a3eb306c62b7
SobhanSoqandi <sobhansoqandi@gmail.com>
2025-09-02 00:45:04 +0330

    sidebar items

--- src/component/UI/SearchInput.jsx
+++ src/component/UI/SearchInput.jsx
@@ -5,12 +5,15 @@ function SearchInput() {
     return (
         <div className="relative flex ">
 
-            <BiSearch className="absolute w-5 h-5 top-2.5 left-2.5 text-blue-500" />
+            <form>
 
-            <input
-                className="w-full bg-transparent border-none focus:border-none focus:outline-none focus:ring-0 rounded-md pl-10 pr-3 py-2 "
-                placeholder="Queck Search"
-            />
+                <BiSearch className="absolute w-5 h-5 top-2.5 left-2.5 text-blue-500" />
+
+                <input
+                    className="w-full bg-transparent border-none focus:border-none focus:outline-none focus:ring-0 rounded-md pl-10 pr-3 py-2 "
+                    placeholder="Queck Search"
+                />
+            </form>
 
         </div>
     )
